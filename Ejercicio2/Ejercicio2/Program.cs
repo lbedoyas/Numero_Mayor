27d617139f962b3c9f956c9b1bd5f0cd4cc4235e
LB <lucho.bedoya1@hotmail.com>
2018-01-18 18:40:01 -0500

    Numero_Mayor
    Programa que pida por pantalla 3 números y luego diga cuál es el mayor de los números ingresados.

---
+++ Ejercicio2/Ejercicio2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c;
+            Console.Write("Ingrese el primer numero");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Ingrese el segundo numero");
+            b = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Ingrese el tercer numero");
+            c = Convert.ToInt32(Console.ReadLine());
+
+            if(a > b && a > c)
+            {
+                Console.WriteLine("El numero mayor es: " + a);
+                Console.ReadKey();
+            }
+            else if( b > a && b > c)
+            {
+                Console.WriteLine("El numero mayor es:  " + b);
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine("El numero mayor es:  " + c);
+                Console.ReadKey();
+            }
+
+        }
+    }
+}
